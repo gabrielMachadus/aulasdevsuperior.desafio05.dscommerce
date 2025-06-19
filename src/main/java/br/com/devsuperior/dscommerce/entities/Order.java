9fe8e73104412fab06488caa2cbf796e3a4e9fe0
MachadoGabriel97 <6249824@aluno.uniasselvi.com.br>
2025-06-19 17:09:54 -0300

    Alteração de nome do campo order status e adição dos getter e setter do lombok

--- src/main/java/br/com/devsuperior/dscommerce/entities/Order.java
+++ src/main/java/br/com/devsuperior/dscommerce/entities/Order.java
@@ -3,6 +3,8 @@ package br.com.devsuperior.dscommerce.entities;
 import br.com.devsuperior.dscommerce.enums.OrderStatus;
 import jakarta.persistence.*;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.Instant;
 import java.util.HashSet;
@@ -14,82 +16,47 @@ import java.util.Set;
 @Table(name = "tb_order")
 @EqualsAndHashCode
 public class Order {
+    @Getter
+    @Setter
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Getter
+    @Setter
     @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant moment;
 
-    private OrderStatus orderStatus;
+    @Setter
+    @Getter
+    private OrderStatus status;
 
+    @Setter
+    @Getter
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
 
+    @Setter
+    @Getter
     @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
     private Payment payment;
 
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Instant getMoment() {
-        return moment;
-    }
-
-    public void setMoment(Instant moment) {
-        this.moment = moment;
-    }
-
-    public OrderStatus getOrderStatus() {
-        return orderStatus;
-    }
-
-    public void setOrderStatus(OrderStatus orderStatus) {
-        this.orderStatus = orderStatus;
-    }
-
-    public User getClient() {
-        return client;
-    }
-
-    public void setClient(User client) {
-        this.client = client;
-    }
-
-    public Payment getPayment() {
-        return payment;
-    }
-
-    public void setPayment(Payment payment) {
-        this.payment = payment;
-    }
-
+    @Getter
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new HashSet<>();
 
-    public Set<OrderItem> getItems() {
-        return items;
-    }
-
     public List<Product> getProducts(){
-        return items.stream().map(x -> x.getProduct()).toList();
+        return items.stream().map(OrderItem::getProduct).toList();
     }
 
     public Order() {
     }
 
-    public Order(Long id, Instant moment, OrderStatus orderStatus, User client, Payment payment) {
+    public Order(Long id, Instant moment, OrderStatus status, User client, Payment payment) {
         this.id = id;
         this.moment = moment;
-        this.orderStatus = orderStatus;
+        this.status = status;
         this.client = client;
         this.payment = payment;
     }
