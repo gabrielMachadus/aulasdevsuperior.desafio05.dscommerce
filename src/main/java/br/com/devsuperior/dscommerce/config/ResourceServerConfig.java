5a4cf41b55bad1fce5e7a1981ad8e08c48f167ce
MachadoGabriel97 <6249824@aluno.uniasselvi.com.br>
2025-06-30 12:06:47 -0300

    fix: correção do erro 403 ao chamar o endpoint (users/me). Adicionado novo metodo para alteração do claim name para authorities.

--- src/main/java/br/com/devsuperior/dscommerce/config/ResourceServerConfig.java
+++ src/main/java/br/com/devsuperior/dscommerce/config/ResourceServerConfig.java
@@ -1,17 +1,21 @@
 package br.com.devsuperior.dscommerce.config;
 
 import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -52,8 +56,18 @@ public class ResourceServerConfig {
 				.requestMatchers("/orders/**").authenticated()
 				.requestMatchers(HttpMethod.GET, "/users/me").authenticated()
 				.anyRequest().permitAll());
+		http.oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())));
 
 		return http.build();
+	}
+
+	private Converter<Jwt, ? extends AbstractAuthenticationToken> jwtAuthenticationConverter() {
+		JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+		JwtGrantedAuthoritiesConverter authoritiesConverter = new JwtGrantedAuthoritiesConverter();
+		authoritiesConverter.setAuthoritiesClaimName("authorities");
+		authoritiesConverter.setAuthorityPrefix("");
+		converter.setJwtGrantedAuthoritiesConverter(authoritiesConverter);
+		return converter;
 	}
 
 	@Bean
